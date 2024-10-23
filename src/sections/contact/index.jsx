cf2edeab14cce9fd66ecca1619acd78e15930b3c
Adam S <aszlag@sokrates.pl>
2024-10-23 12:53:42 +0200

    feat/add logic to controlled form, add styles matching mockup in figma

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,23 +1,49 @@
+import { useState } from 'react';
 import './index.css';
 import Heading from '../../components/heading';
 import Text from '../../components/text';
+
 const Contact = () => {
+  //TODO: and types
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    service: ''
+  });
+
+  //TODO: Add validation and error handling
+  //TODO: add global variables in CSS + reusuable classes
+  //TODO: add tests
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    //TODO: add functionality to submit form
+    e.preventDefault();
+    console.log('formData', formData);
+  };
 
   return (
     <div className="contact" id="contact">
       <div className='container'>
-        <div className="contact__content">
+        <div className="contact__left">
           <Heading level={2}>Contact</Heading>
           <Text>
-            Kappa
+            Questions or concerns? Just fill out the form below and our support team will get back to you within 24 hours
           </Text>
         </div>
         <div className='contact__form'>
-          <form>
-            <input type="text" placeholder='Name' />
-            <input type="email" placeholder='Email' />
-            <textarea placeholder='Message'></textarea>
-          </form>
+          <div>
+            <input type="text" name='firstName' value={formData.firstName} onChange={handleChange} placeholder='First Name' />
+            <input type="text" name='lastName' value={formData.lastName} onChange={handleChange} placeholder='Last Name' />
+          </div>
+          <input type="tel" name='phoneNumber' value={formData.phoneNumber} onChange={handleChange} placeholder='Phone Number' />
+          <textarea name='service' value={formData.service} onChange={handleChange} placeholder='What Service are you interested in?'></textarea>
+          <button className="btn" type='submit' onClick={handleSubmit}>Submit Now</button>
         </div>
       </div>
     </div>
