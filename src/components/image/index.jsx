b6cc462715b3d8f9290750d70f989f9724212b43
Adam S <aszlag@sokrates.pl>
2024-10-23 13:18:36 +0200

    chore: refactor image component - add isLoaded, create error state and fallback, extend component with styles and ...rest as option

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,11 +1,38 @@
-const Image = ({ src, alt, className, width = 'auto', height = 'auto' }) => {
+import React, { useState } from 'react';
 
-  const handleImageError = (e) => {
-    e.target.alt = 'Image failed to load';
+const Image = ({ src, alt, className, width = 'auto', height = 'auto', loading = 'lazy', style, ...rest }) => {
+  //TODO: add TS
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
   };
 
-  return <img src={src} alt={alt} className={className} width={width} height={height} onError={handleImageError} />;
+  const handleImageError = (e) => {
+    setIsLoaded(true);
+    setIsError(true);
+    e.target.alt = 'Image failed to load';
+    //TODO add error fallback image f.e 'something went wrong'
+  };
 
+  return <>
+  { !isLoaded && <div>Loading...</div>}
+   { !isError ? 
+   <img
+      src={src}
+      alt={alt}
+      className={className}
+      width={width}
+      height={height}
+      loading={loading}
+      onLoad={handleImageLoad}
+      onError={handleImageError}
+      style={style}
+      {...rest} /> :
+      <div>Error</div>
+   }
+  </>
 };
 
 export default Image;
